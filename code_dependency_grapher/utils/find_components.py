6e421672e545d42be807ff4f2819d8b46bbc25af
Elisei Smirnov <eliwhatthe@gmail.com>
2024-06-06 11:44:05 +0000

    include whole import path

--- code_dependency_grapher/utils/find_components.py
+++ code_dependency_grapher/utils/find_components.py
@@ -21,10 +21,21 @@ def extract_components(file_path,
 def extract_components_from_files(file_paths):
     file_components_map = {}
     components_names = []
+    package_components_names = []
     
     for file_path in file_paths:
         components = extract_components(file_path)
+        # Split the path using '/' (or '\\' on Windows) to get the package names
+        packages_with_extension = file_path.split('/')[1:] if '/' in file_path \
+                                    else file_path.rsplit('\\', 1)[1:]
+        packages_with_extension = ".".join(packages_with_extension)
+        packages = packages_with_extension.replace('.py', '')
+        modules = [f"{packages}.{component}" for component in components]
+
         file_components_map[file_path] = components
         components_names.extend(components)
+        package_components_names.extend(modules)
 
-    return file_components_map, components_names
+    return file_components_map, \
+            components_names, \
+            package_components_names
