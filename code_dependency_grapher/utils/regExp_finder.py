db11fc96cf9b39987075f085ce6178226e2c1e1f
Stepan Vagin <step.way.a12@gmail.com>
2024-06-17 09:07:14 +0000

    Added regExpFinder

---
+++ code_dependency_grapher/utils/regExp_finder.py
@@ -0,0 +1,45 @@
+import json
+import re
+import os
+
+class regExpFinder:
+
+    def search(path_to_repo, repo_name, regExpStr : str):
+    
+        try:
+            path_to_repo = os.path.join(path_to_repo, repo_name, "data.json")
+            re.compile(regExpStr)
+            with open(path_to_repo, "r") as file:
+                json_data = json.load(file)
+            
+            components = json_data.get("components", [])
+
+            for component in components:
+                component_name = component.get("component_name", "")
+                match = re.search(regExpStr, component_name)
+                if match:
+                    print(f"Found match '{match.group()}' in component: {component_name}")
+                    return component
+            print("No match found in any component.")
+            return None
+        
+        except FileNotFoundError:
+            print(f"File not found: {path_to_repo}")
+            return None
+        
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+            return None
+        
+        except KeyError as e:
+            print(f"Key error: {e}")
+            return None
+        
+        except re.error:
+            print("Given string is not valid regExp")
+            return None
+    
+
+        
+
+
