0acea911a2d713f4876ed77722a3cee94f6d0156
Elisei Smirnov <eliwhatthe@gmail.com>
2024-06-19 08:22:20 +0000

    added abstract repository processor class

---
+++ code_dependency_grapher/cdg/repository_processors/absract_processor.py
@@ -0,0 +1,8 @@
+from code_dependency_grapher.cdg.repository_processors.repositoty_container import RepositoryContainer
+from abc import ABC, abstractmethod
+
+class RepositoryProcessor(ABC):
+
+    @abstractmethod
+    def process(self, repository_container: RepositoryContainer):
+        pass
