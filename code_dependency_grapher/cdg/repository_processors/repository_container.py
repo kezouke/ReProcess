da2a6329d7f32fee50944647ccd0e49d8faaf8cd
Stepan Vagin <step.way.a12@gmail.com>
2024-06-19 12:02:44 +0000

    Added json deconverter

--- code_dependency_grapher/cdg/repository_processors/repository_container.py
+++ code_dependency_grapher/cdg/repository_processors/repository_container.py
@@ -1,9 +1,12 @@
 class RepositoryContainer:
 
-    def __init__(self, repo_name: str, repo_path: str, db_path: str) -> None:
+    def __init__(self, repo_name: str, repo_path: str, repo_author: str, repo_hash:str,
+                 db_path: str) -> None:
 
         self.code_components = []
         self.files = []
         self.repo_name = repo_name
         self.repo_path = repo_path
         self.db_path = db_path
+        self.repo_author = repo_author
+        self.repo_hash = repo_hash
