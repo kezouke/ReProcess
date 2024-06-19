4bba2aa90eef275b3e23beca91942e4a0dff718f
Elisei Smirnov <61423871+kezouke@users.noreply.github.com>
2024-06-19 16:54:44 +0300

    Merge Processors (#12)
    * Addeed Compose function
    
    * yafp stylefix
    
    * ruff fix
    
    ---------
    
    Co-authored-by: Stepan Vagin <step.way.a12@gmail.com>

---
+++ code_dependency_grapher/cdg/repository_processors/processors_compose.py
@@ -0,0 +1,10 @@
+from code_dependency_grapher.cdg.repository_processors.abstract_processor import RepositoryProcessor
+from code_dependency_grapher.cdg.repository_processors.repository_container import RepositoryContainer
+
+
+class Compose:
+
+    def __init__(self, repository_container: RepositoryContainer,
+                 composeList: list[RepositoryProcessor]):
+        for processor in composeList:
+            processor.process(repository_container)
