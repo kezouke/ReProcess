18c7a9e67ad7d891f86e59df226adb7fa84d13f3
Elisei Smirnov <61423871+kezouke@users.noreply.github.com>
2024-06-19 15:09:08 +0300

    Merge repository processors (#11)
    * graph updater basic implementation
    
    * graph updater big fix
    
    * code style improve
    
    * ruff fix

---
+++ code_dependency_grapher/cdg/repository_processors/graph_updater.py
@@ -0,0 +1,138 @@
+import uuid
+from code_dependency_grapher.cdg.requests_handling.RepositoryManager import RepositoryManager
+from code_dependency_grapher.cdg.CodeComponent import CodeComponent
+from code_dependency_grapher.utils.mappers.FilePathAstMapper import FilePathAstMapper
+from code_dependency_grapher.utils.mappers.IdComponentMapper import IdComponentMapper
+from code_dependency_grapher.utils.mappers.IdFileAnalyzerMapper import IdFileAnalyzerMapper
+from code_dependency_grapher.utils.find_components import extract_components_from_files
+from code_dependency_grapher.cdg.repository_processors.abstract_processor import RepositoryProcessor
+from code_dependency_grapher.cdg.repository_processors.repository_container import RepositoryContainer
+
+
+class GraphUpdater(RepositoryProcessor):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def process(self, repository_container: RepositoryContainer):
+
+        changed_files = RepositoryManager(
+            repository_directory=repository_container.repo_path,
+            preprocess=False).get_changed_files(repository_container.repo_path)
+        status_file_name = [line.split('\t') for line in changed_files]
+
+        print(status_file_name)
+        removed_files = [
+            repository_container.repo_path + "/" + line[1]
+            for line in status_file_name if line[0] == 'D'
+        ]
+        updated_files = [
+            repository_container.repo_path + "/" + line[1]
+            for line in status_file_name if line[0] != 'D'
+        ]
+        updated_files_relative_paths = [
+            line[1] for line in status_file_name if line[0] != 'D'
+        ]
+
+        print("Removed files")
+        print(removed_files)
+        print("\nUpdated files")
+        print(updated_files)
+
+        removed_file_ids = []
+        updated_files_ids = []
+        for file in repository_container.files:
+            if file.file_path in removed_files:
+                removed_file_ids.append(file.file_id)
+            if file.file_path in updated_files_relative_paths:
+                updated_files_ids.append(file.file_id)
+
+        repository_container.files = [
+            file for file in repository_container.files
+            if file.file_id not in removed_file_ids
+        ]
+
+        print()
+        print(removed_file_ids)
+        print()
+        print(updated_files_ids)
+
+        removed_components_ids = []
+        updated_components_ids = []
+        for code_component in repository_container.code_components:
+            if code_component.file_analyzer_id in removed_file_ids:
+                removed_components_ids.append(code_component.component_id)
+            if code_component.file_analyzer_id in updated_files_ids:
+                updated_components_ids.append(code_component.component_id)
+
+        repository_container.code_components = [
+            code_component
+            for code_component in repository_container.code_components
+            if code_component.component_id not in removed_components_ids
+            or code_component.component_id not in updated_components_ids
+        ]
+
+        changed_components_ids = set(removed_components_ids +
+                                     updated_components_ids)
+        for code_component in repository_container.code_components:
+            code_component.linked_component_ids = \
+                set(code_component.linked_component_ids).difference(changed_components_ids)
+
+        # Map file paths to their abstract syntax trees (ASTs)
+        ast_manager = FilePathAstMapper(repository_container.repo_path,
+                                        updated_files)
+
+        # Extract components from the found Python files
+        file_components_map, _, package_components_names = extract_components_from_files(
+            updated_files, repository_container.repo_path,
+            ast_manager.file_path_ast_map)
+
+        # Map component identifiers to their corresponding components
+        id_component_manager = IdComponentMapper(
+            repository_container.repo_path, file_components_map)
+
+        # Analyze files to map them to their unique identifiers and other relevant data
+        id_files_manager = IdFileAnalyzerMapper(updated_files, ast_manager,
+                                                package_components_names,
+                                                repository_container.repo_path)
+
+        # Initialize a list to hold the constructed code components
+        code_components = []
+
+        # Construct code components based on the identified components and their relationships
+        for cmp_id in id_component_manager.id_component_map:
+            code_components.append(
+                CodeComponent(cmp_id, repository_container.repo_path,
+                              id_files_manager, ast_manager.file_path_ast_map,
+                              id_component_manager.id_component_map,
+                              package_components_names))
+
+        # Identify external components and link internal components based on imports
+        external_components_dict = {}
+
+        all_packages = [
+            cmp.component_name for cmp in repository_container.code_components
+        ]
+        all_internal_components = set(package_components_names + all_packages)
+        for cmp in code_components:
+            cmp_imports = set(cmp.extract_imports())
+            linked_components = all_internal_components.intersection(
+                cmp_imports)
+            external_components = cmp_imports.difference(linked_components)
+
+            for l_cmp in linked_components:
+                l_cmp_id = id_component_manager.component_id_map[l_cmp]
+                cmp.linked_component_ids.append(l_cmp_id)
+
+            for e_cmp in external_components:
+                e_id = external_components_dict.get(e_cmp, None)
+                if e_id is None:
+                    e_id = str(uuid.uuid4())
+                    external_components_dict[e_cmp] = e_id
+                cmp.external_component_ids.append(e_id)
+
+        # Populate the repository container with the constructed code components and files
+        repository_container.code_components += code_components
+        repository_container.files += [
+            value for _, value in id_files_manager.id_file_map.items()
+        ]
--- code_dependency_grapher/usage_examples/build_dependency_graph.py
+++ code_dependency_grapher/usage_examples/build_dependency_graph.py
@@ -1,11 +1,8 @@
-from code_dependency_grapher.cdg.Engine import Engine
-from code_dependency_grapher.cdg.JsonDeconverter import JsonDeconverter
-from code_dependency_grapher.cdg.repository_processors.repository_container import RepositoryContainer
+# from code_dependency_grapher.cdg.Engine import Engine
 
-
-# Example usage
-db_abs_path = "/home/db"
-path_where_to_store_repos = "/home"
+# # Example usage
+# db_abs_path = "/home/db"
+# path_where_to_store_repos = "/home"
 # engine = Engine(db_abs_path, path_where_to_store_repos)
 
 # engine.clone("https://github.com/arXiv/arxiv-feed")
@@ -14,13 +11,46 @@ path_where_to_store_repos = "/home"
 
 # component = engine.componentSearch("arxiv-feed", r'\bfeed\.routes\.status\b')
 # print(component.component_code)
-to_deconvert = RepositoryContainer("arxiv-feed", "/home/data", "", "", "/home/db")
 
-JsonDeconverter().deconvert(to_deconvert)
+# # Additional example requests commented out for brevity
+# # engine.request("https://github.com/vllm-project/vllm")
+# # engine.request("https://github.com/showpiecep/SQLite_PyQt")
+# =======
+# from code_dependency_grapher.cdg.Engine import Engine
+# from code_dependency_grapher.cdg.JsonDeconverter import JsonDeconverter
+# from code_dependency_grapher.cdg.repository_processors.repository_container import RepositoryContainer
 
-print(to_deconvert.code_components[0])
-print(to_deconvert.files[-1])
-# Additional example requests commented out for brevity
-# engine.request("https://github.com/vllm-project/vllm")
-# engine.request("https://github.com/showpiecep/SQLite_PyQt")
-# engine.request("https://github.com/IU-Capstone-Project-2024/SayNoMore")
+
+# # Example usage
+# db_abs_path = "/home/db"
+# path_where_to_store_repos = "/home"
+# # engine = Engine(db_abs_path, path_where_to_store_repos)
+
+# # engine.clone("https://github.com/arXiv/arxiv-feed")
+
+# # engine.request(repo_folder_name="arxiv-feed")
+
+# # component = engine.componentSearch("arxiv-feed", r'\bfeed\.routes\.status\b')
+# # print(component.component_code)
+# to_deconvert = RepositoryContainer("arxiv-feed", "/home/data", "", "", "/home/db")
+
+# JsonDeconverter().deconvert(to_deconvert)
+
+# print(to_deconvert.code_components[0])
+# print(to_deconvert.files[-1])
+# # Additional example requests commented out for brevity
+# # engine.request("https://github.com/vllm-project/vllm")
+# # engine.request("https://github.com/showpiecep/SQLite_PyQt")
+# >>>>>>> b-053
+# # engine.request("https://github.com/IU-Capstone-Project-2024/SayNoMore")
+
+from code_dependency_grapher.cdg.repository_processors.repository_container import RepositoryContainer
+from code_dependency_grapher.cdg.repository_processors.graph_updater import GraphUpdater
+
+repo_container = RepositoryContainer("arxiv-feed", "/home/arxiv-feed",
+                                     "home/db")
+GraphUpdater().process(repo_container)
+# for cmp in repo_container.code_components:
+#     print(cmp.component_id)
+#     print(cmp.linked_component_ids)
+#     print(cmp.component_name)
