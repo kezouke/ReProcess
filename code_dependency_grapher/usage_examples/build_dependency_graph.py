06e8348c462e86e764f86f9e4032dd14689a6e26
Elisei Smirnov <61423871+kezouke@users.noreply.github.com>
2024-06-20 13:43:09 +0300

    Updated README (#15)
    * Updated README
    
    * yapf stylefix
    
    ---------
    
    Co-authored-by: Stepan Vagin <step.way.a12@gmail.com>

--- code_dependency_grapher/usage_examples/build_dependency_graph.py
+++ code_dependency_grapher/usage_examples/build_dependency_graph.py
@@ -1,5 +1,9 @@
-from code_dependency_grapher.cdg.repository_processors import JsonDeconverter, GraphUpdater, JsonConverter, RepositoryContainer, Compose
+from code_dependency_grapher.cdg.repository_processors import GraphBuilder, JsonConverter, RepositoryContainer, Compose, CloneRepository
 
 repo_container = RepositoryContainer("arxiv-feed", "/home/arxiv-feed",
                                      "/home/db")
-Compose(repo_container, [JsonDeconverter(), GraphUpdater(), JsonConverter()])
+Compose(repo_container, [
+    CloneRepository("https://github.com/arXiv/arxiv-feed"),
+    GraphBuilder(),
+    JsonConverter()
+])
