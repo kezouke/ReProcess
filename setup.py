31c618eb540ed12e1826c70d3ddb75c2361ed37f
Elisei Smirnov <eliwhatthe@gmail.com>
2024-06-20 12:27:59 +0000

    code style improve

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 from setuptools import setup, find_packages
 
-setup(
-    name='code_dependency_grapher',
-    version='0.1',
-    description='A tool for generating dependency graphs from code repositories.',
-    author='Advanced Engineering School',
-    author_email='eliwhatthe@gmail.com',
-    packages=find_packages(),
-    package_data={
-        'data': ['path.json'],
-    },
-    url='https://github.com/kezouke/TestGena',
-    license='MIT',
-    keywords='dependency-graph code-analysis',
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
-    ], 
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown'
-)
+setup(name='code_dependency_grapher',
+      version='0.1',
+      description=
+      'A tool for generating dependency graphs from code repositories.',
+      author='Advanced Engineering School',
+      author_email='eliwhatthe@gmail.com',
+      packages=find_packages(),
+      package_data={
+          'data': ['path.json'],
+      },
+      url='https://github.com/kezouke/TestGena',
+      license='MIT',
+      keywords='dependency-graph code-analysis',
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.9',
+      ],
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown')
