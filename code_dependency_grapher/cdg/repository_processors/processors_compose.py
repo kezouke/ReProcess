78055fe73a814357ba423b6e5dc32e7d6ebdb3d3
Stepan Vagin <step.way.a12@gmail.com>
2024-06-20 09:40:27 +0000

    yapf stylefix

--- code_dependency_grapher/cdg/repository_processors/processors_compose.py
+++ code_dependency_grapher/cdg/repository_processors/processors_compose.py
@@ -5,8 +5,10 @@ from code_dependency_grapher.cdg.repository_processors.repository_container impo
 
 class Compose:
 
-    def __init__(self, repository_container: RepositoryContainer,
-                 composeList: list[RepositoryProcessor], in_place: bool = True):
+    def __init__(self,
+                 repository_container: RepositoryContainer,
+                 composeList: list[RepositoryProcessor],
+                 in_place: bool = True):
         self.repository_container = repository_container
         self.compose_list = composeList
         self.in_place = in_place
@@ -15,13 +17,12 @@ class Compose:
             for processor in composeList:
                 processor.process(repository_container)
         else:
-            self.deep_copied_repository_container = copy.deepcopy(repository_container)
+            self.deep_copied_repository_container = copy.deepcopy(
+                repository_container)
             for processor in composeList:
                 processor.process(self.deep_copied_repository_container)
-            
+
     def get_processed_container(self) -> RepositoryContainer:
         if not self.in_place:
             return self.deep_copied_repository_container
         return self.repository_container
-            
-            
