db72eb098015bb55d2269e6af6201b5cc937897d
Delviet <daniillarapov@gmail.com>
2024-06-23 01:12:44 +0300

    Minor changes to satisfy YAPF

--- code_dependency_grapher/cdg/repository_processors/compose.py
+++ code_dependency_grapher/cdg/repository_processors/compose.py
@@ -3,6 +3,7 @@ from code_dependency_grapher.cdg.repository_processors.abstract_processor import
 from code_dependency_grapher.cdg.repository_processors.repository_container import RepositoryContainer
 from typing import List
 
+
 class Compose(RepositoryProcessor):
 
     def __init__(self,
@@ -14,7 +15,7 @@ class Compose(RepositoryProcessor):
     def __call__(self, repository_container: RepositoryContainer):
         active_container = repository_container if self.in_place \
                             else copy.deepcopy(repository_container)
-        
+
         for processor in self.processor_list:
             processor(active_container)
 
