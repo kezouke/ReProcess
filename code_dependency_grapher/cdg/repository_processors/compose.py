b00e7694c2e59d699e2215a63aac830ece675e7c
Daniil Arapov <59310708+Delviet@users.noreply.github.com>
2024-06-24 12:53:28 +0300

    [068] Rewrite Compose logics (#20)
    * Rewrite Compose logic, make it RepoProcessor too
    
    * Minor changes to satisfy YAPF
    
    * Minor typo fix

---
+++ code_dependency_grapher/cdg/repository_processors/compose.py
@@ -0,0 +1,22 @@
+import copy
+from code_dependency_grapher.cdg.repository_processors.abstract_processor import RepositoryProcessor
+from code_dependency_grapher.cdg.repository_processors.repository_container import RepositoryContainer
+from typing import List
+
+
+class Compose(RepositoryProcessor):
+
+    def __init__(self,
+                 processor_list: List[RepositoryProcessor],
+                 inplace: bool = False):
+        self.processor_list = processor_list
+        self.inplace = inplace
+
+    def __call__(self, repository_container: RepositoryContainer):
+        active_container = repository_container if self.inplace \
+                            else copy.deepcopy(repository_container)
+
+        for processor in self.processor_list:
+            processor(active_container)
+
+        return active_container
