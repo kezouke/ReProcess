5c0d8f5e42e4f93fab2f4b8123adceddaa8d5751
Stepan Vagin <98709715+StepanVagin@users.noreply.github.com>
2024-06-25 09:41:11 +0300

    Update clone repository (#22)

--- code_dependency_grapher/cdg/repository_processors/__init__.py
+++ code_dependency_grapher/cdg/repository_processors/__init__.py
@@ -3,7 +3,7 @@ from .graph_builder import GraphBuilder
 from .graph_updater import GraphUpdater
 from .json_converter import JsonConverter
 from .json_deconverter import JsonDeconverter
-from .processors_compose import Compose
+from .compose import Compose
 from .clone_repository import CloneRepository
 
 if __file__ == "__main__":
