28ed9f465476e3379942c7bbb8295401f4bdd0c0
Elisei Smirnov <el.smirnov@innopolis.university>
2024-07-03 21:00:22 +0800

    ruff fix

--- code_dependency_grapher/cdg/repository_processors/regExp_finder.py
+++ code_dependency_grapher/cdg/repository_processors/regExp_finder.py
@@ -1,6 +1,5 @@
 import json
 import re
-import os
 from code_dependency_grapher.cdg.repository_processors.abstract_processor import RepositoryProcessor
 from code_dependency_grapher.cdg.repository_processors.repository_container import RepositoryContainer
 
@@ -26,9 +25,6 @@ class RegExpFinder(RepositoryProcessor):
                     found_components.append(component)
 
             return {self.regExpStr: found_components}
-
-        except FileNotFoundError:
-            raise FileNotFoundError(f"File not found: {path_to_repo}")
 
         except json.JSONDecodeError as e:
             print(f"Error decoding JSON: {e}")
