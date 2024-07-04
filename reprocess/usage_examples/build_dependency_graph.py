037f09d88e467ccf8b35881b7ecb1dd0fe7f5138
Elisei Smirnov <el.smirnov@innopolis.university>
2024-07-04 17:32:35 +0800

    code style improve

--- reprocess/usage_examples/build_dependency_graph.py
+++ reprocess/usage_examples/build_dependency_graph.py
@@ -1,7 +1,6 @@
 from reprocess.repository_processors import JsonConverter, ReContainer, GraphBuilder, CloneRepository, Compose, RegExpFinder
 
-repo_container = ReContainer("arxiv-feed", "/home/arxiv-feed",
-                                     "/home/db")
+repo_container = ReContainer("arxiv-feed", "/home/arxiv-feed", "/home/db")
 composition = Compose([
     CloneRepository("https://github.com/arXiv/arxiv-feed"),
     GraphBuilder(),
