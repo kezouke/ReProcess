4837443cba9e7e050cbce997b6119e969059ef35
Elisei Smirnov <el.smirnov@innopolis.university>
2024-07-06 16:04:24 +0800

    modify error message for the assertion

--- reprocess/utils/attribute_linker.py
+++ reprocess/utils/attribute_linker.py
@@ -34,7 +34,7 @@ class AttributeLinker:
     def get_classes_by_attrs(self, attr_list):
         assert all([
             attr_name in self.attrs_to_class.keys() for attr_name in attr_list
-        ]), "Attribute is not registered for any RepositoryProcessor"
+        ]), f"Attributes {attr_list} must be created in at least one ReProcessor class."
 
         class_dict = dict()
         for attr_name in attr_list:
