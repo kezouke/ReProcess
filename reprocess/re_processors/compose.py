974b5c88a970b8983666dda6d60878cafbcc70a8
Elisei Smirnov <el.smirnov@innopolis.university>
2024-07-10 20:30:54 +0800

    AsyncCompose implementation

--- reprocess/re_processors/compose.py
+++ reprocess/re_processors/compose.py
@@ -1,7 +1,8 @@
-from reprocess.re_processors.processor import ReProcessor
+from reprocess.re_processors.processor import ReProcessor, AsyncReProcessor
 from reprocess.re_container import ReContainer
 from typing import List
-
+from copy import deepcopy
+import asyncio
 
 class Compose():
 
@@ -14,3 +15,21 @@ class Compose():
             repository_container = processor(repository_container)
 
         return repository_container
+    
+
+class AsyncCompose(Compose):
+
+    async def __call__(self, repository_container: AsyncReProcessor):
+        tasks = [
+            processor(deepcopy(repository_container))
+            for processor in self.processor_list
+        ]
+        results = await asyncio.gather(*tasks)
+
+        # Merge the results back into the original container
+        for result in results:
+            for key, value in result.items():
+                setattr(repository_container, key, value)
+
+        return repository_container
+
