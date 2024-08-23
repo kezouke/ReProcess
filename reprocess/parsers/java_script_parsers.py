cfd0d75335ff36a07fc13151c034aba9a4ae2960
Elisei Smirnov <eliwhatthe@gmail.com>
2024-08-23 12:18:50 +0000

    extract_component_names implementation

--- reprocess/parsers/java_script_parsers.py
+++ reprocess/parsers/java_script_parsers.py
@@ -23,11 +23,56 @@ class JavaScriptFileParser(TreeSitterFileParser):
 
         cutted_path = self.file_path.split(self.repo_name)[-1]
         self.packages = get_import_statement_path(
-            cutted_path.replace(".go", ""))
+            cutted_path.replace(".js", ""))
         self.file_path = cutted_path[1:]
 
     def extract_component_names(self):
-        return super().extract_component_names()
+        """
+        Extracts component names such as classes, functions, and methods
+        from the JavaScript code.
+        """
+        components = []
+
+        # Helper function to traverse nodes
+        def traverse(node, prefix=""):
+            # Check for class declarations
+            if node.type == "class_declaration":
+                class_name_node = node.child_by_field_name("name")
+                if class_name_node:
+                    class_name = self.source_code[
+                        class_name_node.start_byte:class_name_node.end_byte]
+                    components.append(class_name)
+                    # Traverse class body to find methods
+                    class_body_node = node.child_by_field_name("body")
+                    if class_body_node:
+                        for child in class_body_node.children:
+                            if child.type == "method_definition":
+                                method_name_node = child.child_by_field_name(
+                                    "name")
+                                if method_name_node:
+                                    method_name = self.source_code[
+                                        method_name_node.
+                                        start_byte:method_name_node.end_byte]
+                                    components.append(
+                                        f"{class_name}.{method_name}")
+
+            # Check for function declarations
+            elif node.type == "function_declaration":
+                function_name_node = node.child_by_field_name("name")
+                if function_name_node:
+                    function_name = self.source_code[
+                        function_name_node.start_byte:function_name_node.
+                        end_byte]
+                    components.append(function_name)
+
+            # Recursively traverse children
+            for child in node.children:
+                traverse(child)
+
+        # Start traversing from the root node
+        traverse(self.tree.root_node)
+
+        return components
 
     def extract_called_components(self):
         return super().extract_called_components()
---
+++ reprocess/usage_examples/parsers_usage/javascript_parsers_usage_example.py
@@ -0,0 +1,23 @@
+from reprocess.parsers.java_script_parsers import JavaScriptFileParser, JavaScriptComponentFillerHelper
+
+file_path = "/home/arxiv-feed/feed/test.js"
+parser = JavaScriptFileParser(file_path, "arxiv-feed")
+
+print("Component Names:")
+print(parser.extract_component_names())
+
+# print("\nCalled Components:")
+# print(parser.extract_called_components())
+
+# print("\nImports:")
+# print(parser.extract_imports())
+
+# print("\nCallable Components:")
+# print(parser.extract_callable_components())
+
+# helper = GoComponentFillerHelper("feed.test.main", file_path, parser)
+# print("\nComponent Code:")
+# print(helper.extract_component_code())
+
+# print("\nCallabale components")
+# print(helper.extract_callable_objects())
