51d8fe851b84d342f706f88cbb442f1ecafd5286
Elisei Smirnov <eliwhatthe@gmail.com>
2024-09-23 07:20:42 +0000

    cpp signature

--- reprocess/usage_examples/parsers_usage/java_parsers_usage_example.py
+++ reprocess/usage_examples/parsers_usage/java_parsers_usage_example.py
@@ -15,8 +15,7 @@ print(parser.extract_called_components())
 print("\nCallable components:")
 print(parser.extract_callable_components())
 
-helper = JavaComponentFillerHelper("feed.main.java.Main", file_path,
-                                   parser)
+helper = JavaComponentFillerHelper("feed.main.java.Main", file_path, parser)
 print("\nComponent Code:")
 print(helper.extract_component_code())
 
