bc5c092621e483d8cf2badc2167a8bc02fcbeccf
Elisei Smirnov <eliwhatthe@gmail.com>
2024-10-17 07:35:29 +0000

    added example with neo4j

--- reprocess/usage_examples/re_processing_example.py
+++ reprocess/usage_examples/re_processing_example.py
@@ -4,23 +4,24 @@ from reprocess.re_container import ReContainer
 # Initialize a ReContainer object with the name of the repository,
 # the path where the repository will be cloned,
 # and the path where the JSON graphs will be saved.
-repo_container = ReContainer("arxiv-feed", "/home/arxiv-feed", "/home/db")
+repo_container = ReContainer("booking", "/home/projects/booking", "/home/db")
 
-NEO4J_URI = "bolt://localhost:7299"
-NEO4J_USERNAME = "neo4j"
-NEO4J_PASSWORD = "rerfhfrf"
+# Neo4j connection details
+NEO4J_URI = "bolt://localhost:7299"          # URI for the Neo4j database
+NEO4J_USERNAME = "neo4j"                     # Username for Neo4j login
+NEO4J_PASSWORD = "password"                  # Password for Neo4j login
 
 # Create a Compose object that specifies a sequence of operations
 # to be performed on the repository. This sequence includes cloning
 # the repository, building a dependency graph, searching for components
 # matching a regex pattern, and converting the repository data
-# to JSON format.
+# to JSON format,  and integrating the repository data with Neo4j 
+# for graph storage and querying.
 composition = Compose([
-    CloneRepository("https://github.com/arXiv/arxiv-feed"),
     GraphBuilder(),
     RegExpFinder("^(.*test.*)$|^((?:.*[Tt]est).*)$"),
     JsonConverter(),
-    Neo4jConverter(NEO4J_URI, NEO4J_USERNAME, NEO4J_PASSWORD)
+    Neo4jConverter(NEO4J_URI, NEO4J_USERNAME, NEO4J_PASSWORD) # Store the graph data in Neo4j
 ])
 # Execute the sequence of operations on
 # the repository container.
